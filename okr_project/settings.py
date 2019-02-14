a28a940f552658b57f320624a39fae42dddef29b
Alberto Alcolea <albertoalcolea@gmail.com>
2019-02-14 23:33:59 +0100

    Added migration files

--- okr_project/settings.py
+++ okr_project/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE_CLASSES = (
 )
 
 TEMPLATES = [
-{
+    {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
